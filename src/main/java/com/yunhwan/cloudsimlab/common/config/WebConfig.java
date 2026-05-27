252a0feff06ba106083a1745347929b49a9b7190
YUNHWAN <629jyh7@naver.com>
2026-05-27 12:25:03 +0900

    feat: webConfig 추가

---
+++ src/main/java/com/yunhwan/cloudsimlab/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.yunhwan.cloudsimlab.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+				.allowedOrigins("http://localhost:5173")
+				.allowedMethods(
+						HttpMethod.GET.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.OPTIONS.name()
+				)
+				.allowedHeaders(HttpHeaders.CONTENT_TYPE);
+	}
+}
