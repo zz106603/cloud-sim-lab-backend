6e44f1fa0f7f8c7c9b3ee72619078adf97f222a7
YUNHWAN <629jyh7@naver.com>
2026-06-02 10:12:37 +0900

    feat: review 관련 dto, usecase 추가

--- src/main/java/com/yunhwan/cloudsimlab/scenario/application/port/in/GetScenarioUseCase.java
+++ src/main/java/com/yunhwan/cloudsimlab/scenario/application/port/in/GetScenarioUseCase.java
@@ -5,10 +5,13 @@ import java.util.List;
 import com.yunhwan.cloudsimlab.scenario.domain.Scenario;
 import com.yunhwan.cloudsimlab.scenario.domain.ScenarioCategory;
 import com.yunhwan.cloudsimlab.scenario.domain.ScenarioLevel;
+import com.yunhwan.cloudsimlab.scenario.domain.RelatedLearningDocument;
 
 public interface GetScenarioUseCase {
 
 	List<Scenario> findAll(ScenarioCategory category, ScenarioLevel level);
 
 	Scenario findOne(Long scenarioId);
+
+	List<RelatedLearningDocument> findRelatedLearningDocuments(Scenario scenario);
 }
