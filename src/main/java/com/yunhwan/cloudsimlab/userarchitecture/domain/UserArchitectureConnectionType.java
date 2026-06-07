0e2ec597e7024ba426258a423d9eb31d3e02b2ab
YUNHWAN <629jyh7@naver.com>
2026-06-07 13:01:33 +0900

    feat: 사용자 작성 아키텍처 도메인 모델 추가

---
+++ src/main/java/com/yunhwan/cloudsimlab/userarchitecture/domain/UserArchitectureConnectionType.java
@@ -0,0 +1,9 @@
+package com.yunhwan.cloudsimlab.userarchitecture.domain;
+
+public enum UserArchitectureConnectionType {
+	REQUEST_FLOW,
+	NETWORK_ROUTE,
+	DEPENDS_ON,
+	REPLICATION,
+	SECURITY_RULE
+}
